86cdb7f6f5b7dae8e52e6c999f2929d505ea33db
gabriel <gabrielemidenis@gmai.com>
2025-08-18 09:43:14 -0300

    first commit

---
+++ Ejercicios Complementarios.py
@@ -0,0 +1,53 @@
+# \
+numero_1 = 7456
+print("Ejercicio 1: numero 1: ",numero_1)
+
+numero_2 = 3.14
+print("Ejercicio 2: numero 2",numero_2)
+
+suma = numero_1 + numero_2
+print("Ejercicio 3: La suma es: ",suma)
+
+resta = numero_1 - numero_2
+multiplicacion = numero_1 * numero_2
+division = numero_1 / numero_2
+print("Ejercicio 4: Resta: ",resta, "Multiplicacion: ",multiplicacion, "Division: ",division)
+
+nombre = "Gabriel"
+print("Ejercicio 5: nombre: ",nombre)
+
+precio = 69.99
+print("Ejercicio 6: precio: ",precio)
+
+descuento = 0.95
+print("Ejercicio 7: descuento: ",descuento)
+
+precio_final = precio - (precio * descuento)
+print("Ejercicio 8: precio final: ",precio_final)
+
+frase = "Que onda bro"
+print("Ejercicio 9: frase: ",frase)
+
+longitud = len(frase)
+print("Ejercicio 10: longitud: ",longitud)
+
+precio = int(precio)
+print("Ejercicio 11: precio en entero: ",precio)
+
+nombre = "Gabriel"
+apellido = "Denis"
+nombre_completo = nombre + " " + apellido
+print("Ejercicio 12: nombre completo: ",nombre_completo)
+
+edad = (25 + 5) - 10
+print("Ejercicio 13: edad: ",edad)
+
+altura = (1.87 * 4) / 3
+print("Ejercicio 14: altura: ",altura)
+
+nombre = "GABRIEL DENIS"
+nombre_minuscula = nombre.lower()
+print("Ejercicio 15: nombre en minuscula: ",nombre_minuscula)
+
+nombre_capitalizado = nombre.capitalize()
+print("Ejercicio 16: nombre solo con la letra inicial en mayuscula: ",nombre_capitalizado)
